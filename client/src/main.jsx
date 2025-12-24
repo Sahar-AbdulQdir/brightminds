4f73c20e1c1742fd53d663edd9cbcdc8a81813b6
Sahar-AbdulQdir <161846493+Sahar-AbdulQdir@users.noreply.github.com>
2025-12-24 16:48:46 +0400

    Cleaned styles folder

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
+import "./styles/index.css";
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
