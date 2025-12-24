546404aa11a322924820ff44fdb2cf812c88e2dc
Sahar-AbdulQdir <161846493+Sahar-AbdulQdir@users.noreply.github.com>
2025-12-25 00:32:06 +0400

    fix the website to open landing page first

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,10 +14,10 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout><Home /></Layout>} />
+        <Route path="/Home" element={<Layout><Home /></Layout>} />
         <Route path="/Podcasts" element={<Layout><Podcasts /></Layout>} />
         <Route path="/AudioBooks" element={<Layout><AudioBooks /></Layout>} />
-        <Route path="/Landing" element={<LandingPage />} /> 
+        <Route path="/" element={<LandingPage />} /> 
         <Route path="/tools" element={<Layout><ToolsResources /></Layout>} />
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/SavedPage" element={<Layout><SavedPage /></Layout>} />
