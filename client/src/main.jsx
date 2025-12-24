d1e8f5533cb84119289d7e11ee2b668dc53e9fcc
Sahar-AbdulQdir <161846493+Sahar-AbdulQdir@users.noreply.github.com>
2025-12-24 16:51:14 +0400

    Cleaned server folder

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/index.css";
-
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
