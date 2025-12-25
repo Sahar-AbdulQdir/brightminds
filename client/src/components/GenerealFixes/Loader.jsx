ca068b36af747a2f447b49c60b0e1edd85b4dfcf
Sahar-AbdulQdir <161846493+Sahar-AbdulQdir@users.noreply.github.com>
2025-12-25 08:58:30 +0400

    CSS responsivenss styles for home & landing pages

--- client/src/components/GenerealFixes/Loader.jsx
+++ client/src/components/GenerealFixes/Loader.jsx
@@ -5,12 +5,13 @@ import "../../styles/GeneralFixesStyles/Loader.css";
 const Loader = () => {
   return (
     <div className="video-loader-overlay">
-      <video 
-        className="loader-video" 
-        src="/Loader.mp4" // Use URL path starting with /
-        autoPlay 
-        loop 
-        muted 
+      <video
+        className="loader-video"
+        src="/Loader.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
       />
     </div>
   );
